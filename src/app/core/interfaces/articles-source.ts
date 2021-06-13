2dc3709a0d6df22cff21c9588a4e60286b6c8cde
jcayetano <jcayetano@apap.com.do>
2021-06-13 12:01:00 -0400

    Moving response interfaces

--- src/app/core/interfaces/articles-source.ts
+++
@@ -1,4 +0,0 @@
-export interface ArticleSource {
-    id: string
-    name: string
-}
--- src/app/core/interfaces/news-response.ts
+++ src/app/core/interfaces/news-response.ts
@@ -1,7 +1,21 @@
-import { Articles } from "./articles";
-
 export interface NewsResponse{ 
     status: string;
     totalResults: number;
     articles: Array<Articles>;
+}
+
+export interface ArticleSource {
+    id: string
+    name: string
+}
+
+export interface Articles {
+    source: ArticleSource,
+    author: string,
+    title: string,
+    description: string,
+    url: string,
+    urlToImage: string,
+    publishedAt: Date,
+    content: string
 }
